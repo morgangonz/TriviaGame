dd527c1a91c26b6b5dcb2f63276cb7086f2f019c
Morgan Gonzalez <Morgan@Morgans-MacBook-Pro.local>
2016-11-06 19:11:13 -0500

    working questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,87 @@
 $(document).ready(function(){
 
 //array of 5 T/F questions
-var triviaQuestions = 
-				 ['Sharks rarely stop swimming', 
-				'If you never enter the ocean, you are safe from shark attacks.',
-				'Shark attacks are one of the most common events among floridians.',
-				'Great Whites are the worlds largest sharks.',
-				'Sharks are older than dinosuars.',];
+var triviaQuestions = [
+		//q0
+		{
+			question: 'Sharks rarely stop swimming',
+			answer: 'True',
+			message: 'True, most shark species will drown if they stop swimming. The do not have the muscles they need to pump water through their mouth and over their gills.',
+		},
+		//q1
+		{
+			question: 'If you never enter the ocean, you\'re safe from shark attacks.',
+			answer: 'False',
+			message: 'False, Bull sharks, one of the more aggressive species, have a fondness for freshwater. They have been spotted in bays, lagoons and rivers thousands of miles inland.',
+		},
+		//q2
+		{
+			question: 'Shark attacks are one of the most common events among floridians.',
+			answer: 'False',
+			message: 'False, Getting struck by lightning along with many other crazy deaths are more common than getting eaten by a Shark. In reality, sharks are as afraid of us as we are them.',
+		},
+		//q3
+		{
+			question: 'Great Whites are the worlds largest sharks.',
+			answer: 'False',
+			message: 'False, Whale Sharks are the world\'s largest shark species, they can get up to 47,000lbs and grow over 45 feet long. However, they are gentle giants!!!',
+		},
+		//q4
+		{
+			question: 'Sharks are older than dinosuars.',
+			answer: 'True',
+			message: 'True, They have been on the earth for over 400 million years, they predate practically everything that has a spine, including humans and dinosaurs.',
+		}
+];
 
-
-//array of answers to above questions
-var triviaAnswers = 
-				['True, most shark species will drown if they stop swimming. The do not have the muscles they need to pump water through their mouth and over their gills.',
-				'False, Bull sharks, one of the more aggressive species, have a fondness for freshwater. They have been spotted in bays, lagoons and rivers than can be thousands of miles inland.',
-				'False, Getting struck by lightning along with many other crazy deaths are more common than getting eaten by a Shark. IN reality, sharks are just afraid of us as we are them.',
-				'False, Whale Sharks are the world\'s largest shark species, they can get up to 47,000lbs or more and span over 45 feet long. However, they are gentle giants!!!',
-				'True, They have been on the earth for over 400 million years, they predate practically everything that has a spine, including humans and dinosaurs.',];
-
-
-	//to display questions in above array
+//set variables to hold score
 
 	var questionNumber = 0;	
 
-	var currentAnswerVal = false;
+	var correct = 0;
 
-$(".questionDiv").html(triviaQuestions[questionNumber])	
+	var incorrect = 0;
 
-var checkQuestion = function(){
+	var timeout = 0;
 
-		
-			if (questionNumber == triviaQuestions){
-				console.log("correct!");
-				
-				//moves and displays next question
-					questionNumber++;
 
-					$(".questionDiv").html(triviaQuestions[questionNumber])	
-				} else {
-					console.log("Incorrect!");
-					questionNumber++;
-
-				} 
-		
-		$("#trueButton").click(function(){
-			currentAnswerVal = true;
-				checkQuestion(questionNumber);
-		});		
-
-		$("#falseBtn").click(function(){
-			currentAnswerVal = false;
-				checkQuestion(questionNumber);
-			});
+//new fn to display questions into specified div
+questionDisplay();
+function questionDisplay() {
+		$('#answerDiv').empty();
+		$(".questionDiv").html(triviaQuestions[questionNumber].question);	
 	};
+
+
+//when either button is clicked run THIS fn
+$(".btn").on('click', function(){
+	if ($(this).html() == triviaQuestions[questionNumber].answer){
+		//add one to each var
+		questionNumber++
+		correct++
+		answerDisplay()
+	}		else {
+		questionNumber++;
+		incorrect++
+		answerDisplay()
+	}
+});
+
+//displays object answer for 5 seconds
+function answerDisplay() {
+	$(".questionDiv").empty();
+	$('#answerDiv').html(triviaQuestions[questionNumber-1].message);
+	setTimeout(questionDisplay,8000);
+};
 
 
 //timer attempt
 //set counter to 30
 
-
 var timeInterval = 10 
 	
 	//when either button gets clicked run the stop fn
-	$('button').on('click', stop);
+	$('.btn').on('click', stop);
 
 //run fn and decrease by 1 second
 function run(){
@@ -90,6 +110,14 @@ run();
 
 
 
-//closing tag- do not touch
-});
+//this closing tag to line one. 
+	});
+
+
+
+
+
+
+
+
 
